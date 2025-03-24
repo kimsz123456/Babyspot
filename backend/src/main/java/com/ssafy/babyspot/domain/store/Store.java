a88121228fcd36e2546f677e0fc67a49fbae2ece
이상화 <sanghwa1215@naver.com>
2025-03-24 17:53:46 +0900

    feat: store entity 설정

---
+++ backend/src/main/java/com/ssafy/babyspot/domain/store/Store.java
@@ -0,0 +1,60 @@
+package com.ssafy.babyspot.domain.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ssafy.babyspot.domain.reveiw.Review;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "store")
+public class Store {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String title;
+
+	@Column(columnDefinition = "geometry(Point,4326)")
+	private String location;
+
+	private Boolean okZone;
+	private String category;
+	private String address;
+	private String contactNumber;
+	private String transportationConvenience;
+	private String businessHour;
+	private float rating;
+	private int reviewCount;
+
+	private Boolean babyChair;
+	private Boolean babyBed;
+	private Boolean strollerAccess;
+	private Boolean diaperChangingStation;
+	private Boolean nursingRoom;
+
+	private String kidsMenu;
+
+	@OneToMany(mappedBy = "store")
+	private List<StoreMenu> menus = new ArrayList<>();
+
+	@OneToMany(mappedBy = "store")
+	private List<StoreKeyword> keywords = new ArrayList<>();
+
+	@OneToMany(mappedBy = "store")
+	private List<StoreImage> images = new ArrayList<>();
+
+	@OneToMany(mappedBy = "store")
+	private List<Review> reviews = new ArrayList<>();
+}
---
+++ backend/src/main/java/com/ssafy/babyspot/domain/store/StoreKeyword.java
@@ -0,0 +1,24 @@
+package com.ssafy.babyspot.domain.store;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class StoreKeyword {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id", nullable = false)
+	private Store store;
+
+	private String keyword;
+
+	private int count;
+}
