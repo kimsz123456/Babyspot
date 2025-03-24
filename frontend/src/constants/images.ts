a9dd6d7eacbb661f88ca346e6ca3884f9c611d8e
한병현 <gksqudgus777@naver.com>
2025-03-25 00:11:45 +0900

    chore: 아이콘, 이미지 상수처리

---
+++ frontend/src/constants/images.ts
@@ -0,0 +1,5 @@
+import {ImageSourcePropType} from 'react-native';
+
+const IMG_DEFAULT_PROFILE: ImageSourcePropType = require('../../assets/images/default-profile.png');
+
+export {IMG_DEFAULT_PROFILE};
