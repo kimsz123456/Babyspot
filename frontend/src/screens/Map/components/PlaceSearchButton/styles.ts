8f5a2b537cb589fa27ccd0b12e47daca3c6340cd
SONAMJU <skawn1228@gmail.com>
2025-03-26 02:14:58 +0900

    feat: `SearchScreen` 페이지 UI 구현

--- frontend/src/screens/Map/components/PlaceSearchButton/styles.ts
+++ frontend/src/screens/Map/components/PlaceSearchButton/styles.ts
@@ -1,14 +1,16 @@
-import {Text, View} from 'react-native';
+import {Image, Pressable, Text} from 'react-native';
 
 import styled from 'styled-components/native';
 
+import scale from '../../../../utils/scale';
 import {FontStyles} from '../../../../constants/fonts';
 import {GrayColors} from '../../../../constants/colors';
 
-export const PlaceSearchButton = styled(View)`
+export const PlaceSearchButton = styled(Pressable)`
   flex: 1;
   display: flex;
   flex-direction: row;
+  gap: ${scale(4)}px;
   align-items: center;
   padding: 8px;
   border-width: 1px;
@@ -16,6 +18,11 @@ export const PlaceSearchButton = styled(View)`
   border-color: ${GrayColors[200]};
   border-radius: 5px;
   background-color: ${GrayColors[0]};
+`;
+
+export const LeftArrowIcon = styled(Image)`
+  width: ${scale(24)}px;
+  height: ${scale(24)}px;
 `;
 
 export const Placeholder = styled(Text)`
