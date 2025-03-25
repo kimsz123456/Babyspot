55b0896123858491251f4e95282b92315f89c4cb
한병현 <gksqudgus777@naver.com>
2025-03-26 00:27:34 +0900

    feat: 프로필 정보 컨테이너 구현

--- frontend/src/screens/Profile/ProfileScreen/index.tsx
+++ frontend/src/screens/Profile/ProfileScreen/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import * as S from './styles.ts';
+import {IMG_DEFAULT_PROFILE} from '../../../constants/images.ts';
+import {IC_AGE3, IC_PROFILE_EDIT} from '../../../constants/icons.ts';
 
 const ProfileScreen = () => {
-  return <S.Title>프로필</S.Title>;
+  const name = '감귤하우스';
+  return (
+    <S.BackGround>
+      <S.ProfileContainer>
+        {/* api 연동시 실제 사용자 프로필 이미지 */}
+        <S.ProfileImage source={IMG_DEFAULT_PROFILE} />
+        <S.ProfileInfo>
+          <S.NameContainer>
+            <S.Name>{name}</S.Name> 님
+          </S.NameContainer>
+          <S.AgeIcons source={IC_AGE3} />
+        </S.ProfileInfo>
+        <S.ProfileEdit source={IC_PROFILE_EDIT} />
+      </S.ProfileContainer>
+    </S.BackGround>
+  );
 };
 
 export default ProfileScreen;
--- frontend/src/screens/Profile/ProfileScreen/styles.ts
+++ frontend/src/screens/Profile/ProfileScreen/styles.ts
@@ -1,9 +1,60 @@
 import styled from 'styled-components/native';
 import {FontStyles} from '../../../constants/fonts';
-import {SystemColors} from '../../../constants/colors';
-export const Title = styled.Text`
-  font-family: ${FontStyles.displayMedium.fontFamily};
-  font-size: ${FontStyles.displayMedium.fontSize}px;
-  line-height: ${FontStyles.displayMedium.lineHeight}px;
-  color: ${SystemColors.danger};
+import {PrimaryColors} from '../../../constants/colors';
+import {GrayColors} from '../../../constants/colors';
+import scale from '../../../utils/scale';
+import {Image, Text, View} from 'react-native';
+
+const BackGround = styled(View)`
+  flex: 1;
+  background-color: ${GrayColors[0]};
 `;
+
+const ProfileContainer = styled(View)`
+  padding-top: ${scale(32)};
+  padding-left: ${scale(24)};
+  padding-right: ${scale(24)};
+  flex-direction: row;
+  align-items: center;
+`;
+const ProfileImage = styled(Image)`
+  height: ${scale(64)}px;
+  width: ${scale(64)}px;
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: ${GrayColors[100]};
+`;
+const ProfileInfo = styled(View)`
+  margin-left: ${scale(16)};
+  flex: 1;
+`;
+const NameContainer = styled(Text)`
+  ${FontStyles.bodyMedium};
+  color: ${GrayColors[800]};
+  flex: 1;
+  top: 0;
+`;
+const Name = styled(Text)`
+  font-weight: bold;
+  color: ${PrimaryColors[500]};
+`;
+const AgeIcons = styled(Image)`
+  width: ${scale(24)};
+  height: ${scale(24)};
+  botton: 0;
+`;
+const ProfileEdit = styled(Image)`
+  width: ${scale(32)};
+  height: ${scale(32)};
+`;
+
+export {
+  BackGround,
+  ProfileContainer,
+  ProfileImage,
+  ProfileInfo,
+  NameContainer,
+  Name,
+  AgeIcons,
+  ProfileEdit,
+};
