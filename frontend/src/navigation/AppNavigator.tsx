e914d4385699b9786aede370c2f9a2ef3ec5f124
상한규 <nic6@naver.com>
2025-03-25 15:15:42 +0900

    refactor:앱 네비게이터 안 쓰는 로직 삭제

--- frontend/src/navigation/AppNavigator.tsx
+++ frontend/src/navigation/AppNavigator.tsx
@@ -1,21 +1,3 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import MapScreen from '../screens/Map/MapScreen';
-import ProfileScreen from '../screens/Profile/ProfileScreen';
-
-const Stack = createNativeStackNavigator();
-
-const AppNavigator = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Map">
-        <Stack.Screen name="Map" component={MapScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+const AppNavigator = () => {};
 
 export default AppNavigator;
