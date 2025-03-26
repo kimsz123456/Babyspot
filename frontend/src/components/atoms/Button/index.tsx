e0a7f60d23ba42d0f303b12a6189f1205310bacb
한병현 <gksqudgus777@naver.com>
2025-03-26 23:03:41 +0900

    feat: 공통 컴포넌트 메인 버튼 UI구현

---
+++ frontend/src/components/atoms/Button/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './styles';
+
+const ProfileEditButton = () => {
+  return (
+    <S.MainButtonContainer>
+      <S.MainButtonText>프로필 수정</S.MainButtonText>
+    </S.MainButtonContainer>
+  );
+};
+
+export default ProfileEditButton;
---
+++ frontend/src/components/atoms/Button/styles.ts
@@ -0,0 +1,21 @@
+import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
+import {GrayColors, PrimaryColors} from '../../../constants/colors';
+import {FontStyles} from '../../../constants/fonts';
+import scale from '../../../utils/scale';
+
+const MainButtonContainer = styled(View)`
+  width: 100%;
+  border-radius: 10px;
+  background-color: ${PrimaryColors[500]};
+  flex: 1;
+  align-items: center;
+`;
+
+const MainButtonText = styled(Text)`
+  ${FontStyles.bodyMedium};
+  color: ${GrayColors[0]};
+  padding: ${scale(16)}px 0;
+`;
+
+export {MainButtonContainer, MainButtonText};
