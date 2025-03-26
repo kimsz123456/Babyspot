9c5660d2a1ca4a44efbfb54733fa547bfe628540
한병현 <gksqudgus777@naver.com>
2025-03-26 23:28:55 +0900

    feat: 프로필 수정 스크린 UI 구현

---
+++ frontend/src/screens/Profile/ProfileEditScreen/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as S from './styles';
+import ProfileImage from './components/ProfileImage';
+import MyInformation from './components/MyInformation';
+import ChildAge from './components/ChildAge';
+import ProfileEditButton from '../../../components/atoms/Button';
+
+const ProfileEditScreen = () => {
+  return (
+    <S.BackGround>
+      {/* 이미지 추가 연동필요 */}
+      <ProfileImage />
+      <MyInformation />
+      <ChildAge />
+      <S.ProfileEditButtonWrapper>
+        <ProfileEditButton />
+      </S.ProfileEditButtonWrapper>
+    </S.BackGround>
+  );
+};
+
+export default ProfileEditScreen;
