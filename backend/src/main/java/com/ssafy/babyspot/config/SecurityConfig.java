5d3e12259e7f5e7ca54546c2e3b6dbdbcc1338bc
이상화 <sanghwa1215@naver.com>
2025-03-26 11:53:59 +0900

    refactor: cors수정

--- backend/src/main/java/com/ssafy/babyspot/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/babyspot/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
 
 	private CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://j12a607.p.ssafy.io"));
+		configuration.setAllowedOrigins(List.of("http://localhost:8082", "https://j12a607.p.ssafy.io"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
 		configuration.setAllowCredentials(true);
