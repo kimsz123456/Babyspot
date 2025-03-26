1952091d3fb79da3e64b1eafceff64cc72fbcbbf
한병현 <gksqudgus777@naver.com>
2025-03-26 23:20:42 +0900

    feat: 프로필 이미지 컴포넌트 구현

---
+++ frontend/src/screens/Profile/ProfileEditScreen/components/ProfileImage/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native';
+import {Image, View} from 'react-native';
+import scale from '../../../../../utils/scale';
+import {GrayColors} from '../../../../../constants/colors';
+
+const ProfileImageWrapper = styled(View)`
+  align-items: center;
+`;
+const ProfileImageContainer = styled(View)`
+  width: ${scale(120)}px;
+  height: ${scale(120)}px;
+  position: relative;
+`;
+const ProfileImage = styled(Image)`
+  width: ${scale(120)}px;
+  height: ${scale(120)}px;
+  border-radius: ${scale(60)}px;
+  border-width: 1;
+  border-color: ${GrayColors[200]};
+`;
+const ImageEditButton = styled(Image)`
+  width: ${scale(32)}px;
+  height: ${scale(32)}px;
+  border-radius: ${scale(16)}px;
+  border-width: 1px;
+  border-color: ${GrayColors[200]};
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
+
+export {
+  ProfileImageWrapper,
+  ProfileImageContainer,
+  ProfileImage,
+  ImageEditButton,
+};
