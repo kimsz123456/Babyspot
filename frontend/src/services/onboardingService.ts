0e9f0ae2efd58b95814d3dba29d9538256b26bc7
상한규 <nic6@naver.com>
2025-03-27 16:31:47 +0900

    refactor: store 정리 및 service try 적용

--- frontend/src/services/onboardingService.ts
+++ frontend/src/services/onboardingService.ts
@@ -13,16 +13,49 @@ export interface GetTokenByRefreshTokenResponse {
   refreshToken: string;
 }
 
+export interface SignUpRequest {
+  nickname: string;
+  profileImgUrl: string;
+  birthYears: number[];
+}
+
+export interface SignUpResponse {
+  message: string;
+  accessToken: string;
+  refreshToken: string;
+}
+
 export const kakaoLogin = async (
   kakaoAccessToken: string,
 ): Promise<KakaoLoginResponse> => {
-  const result = await api.post('/auth/kakao', {kakaoAccessToken});
-  return result.data;
+  try {
+    const result = await api.post('/auth/kakao', {kakaoAccessToken});
+
+    return result.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const getTokenByRefreshToken =
   async (): Promise<GetTokenByRefreshTokenResponse> => {
-    const result = await api.post('/auth/refresh-token');
+    try {
+      const result = await api.post('/auth/refresh-token');
+
+      return result.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+export const signUp = async (
+  params: SignUpRequest,
+): Promise<SignUpResponse> => {
+  try {
+    const result = await api.post('/memebers/signup', {params});
 
     return result.data;
-  };
+  } catch (error) {
+    throw error;
+  }
+};
