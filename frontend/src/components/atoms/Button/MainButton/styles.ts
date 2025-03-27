20d1b59d5b5d1e66e2e15921e0a94a224821603c
한병현 <gksqudgus777@naver.com>
2025-03-27 10:06:03 +0900

    style: 기존 버튼을 TouchableOpacity로 수정

--- frontend/src/components/atoms/Button/MainButton/styles.ts
+++ frontend/src/components/atoms/Button/MainButton/styles.ts
@@ -1,17 +1,15 @@
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import styled from 'styled-components/native';
 import {GrayColors, PrimaryColors} from '../../../../constants/colors';
 import {FontStyles} from '../../../../constants/fonts';
 import scale from '../../../../utils/scale';
 
-const MainButtonContainer = styled(View)`
+const MainButtonContainer = styled(TouchableOpacity)`
   width: 100%;
   border-radius: 10px;
   background-color: ${PrimaryColors[500]};
-  flex: 1;
   align-items: center;
 `;
-
 const MainButtonText = styled(Text)`
   ${FontStyles.bodyMedium};
   color: ${GrayColors[0]};
