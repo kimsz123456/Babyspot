e7b61a8ad7894fb990b24a318770fa3cc472ff1b
상한규 <nic6@naver.com>
2025-04-01 16:50:25 +0900

    feature:가게 추천 정보 입력 화면 구성

--- frontend/src/screens/Map/MapScreen/components/RecommendButton/index.tsx
+++ frontend/src/screens/Map/MapScreen/components/RecommendButton/index.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 import * as S from './styles';
 
 import {IC_BABY} from '../../../../../constants/icons';
+import {useMapNavigation} from '../../../../../hooks/useNavigationHooks';
 
 const RecommendButton = () => {
+  const navigation = useMapNavigation();
+
   return (
-    <S.RecommendButton>
+    <S.RecommendButton
+      onPress={() => {
+        navigation.navigate('SelectRecommendationAgeScreen');
+      }}>
       <S.BabyIcon source={IC_BABY} />
       <S.RecommendText>추천</S.RecommendText>
     </S.RecommendButton>
