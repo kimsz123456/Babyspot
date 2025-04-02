9640ed6076b5eeaab57e5137e9629d1272a6a82f
이상화 <sanghwa1215@naver.com>
2025-04-02 17:32:49 +0900

    refactor : KeywordReview 의 데이터타입 변경

--- backend/src/main/java/com/ssafy/babyspot/domain/store/KeywordReview.java
+++ backend/src/main/java/com/ssafy/babyspot/domain/store/KeywordReview.java
@@ -1,11 +1,13 @@
 package com.ssafy.babyspot.domain.store;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -27,6 +29,8 @@ public class KeywordReview {
 	private StoreKeyword storeKeyword;
 
 	@Setter
+	@Lob
+	@Column(columnDefinition = "TEXT")
 	private String review;
 
 	private String source;
