ad2568dba4bf9bc58c270d59b5e6cddc8043cfe2
상한규 <nic6@naver.com>
2025-04-02 13:25:21 +0900

    feature: 개인정보 이용약관 webview

--- frontend/src/screens/Profile/ProfileScreen/components/Setting/index.tsx
+++ frontend/src/screens/Profile/ProfileScreen/components/Setting/index.tsx
@@ -4,9 +4,9 @@ import {IC_QUESTION, IC_RIGHT_ARROW} from '../../../../../constants/icons';
 import {ThinDivider} from '../../../../../components/atoms/Divider';
 import {useProfileNavigation} from '../../../../../hooks/useNavigationHooks';
 import VersionCheck from 'react-native-version-check';
-import PrivacyPolicyButton from '../Buttons/PrivacyPolicyButton';
 import {Alert, Linking, PermissionsAndroid} from 'react-native';
 import {SystemColors} from '../../../../../constants/colors';
+import {withDivider} from '../../../../../utils/withDivider';
 
 const Setting = () => {
   const navigation = useProfileNavigation();
@@ -105,45 +105,58 @@ const Setting = () => {
     }
   };
 
+  const handlePrivatePolicyPress = () => {
+    navigation.navigate('PrivacyPolicy');
+  };
+
+  const handleLogoutPress = () => {};
+
   return (
     <S.SettingContainer>
       <S.SettingTitle>설정</S.SettingTitle>
       <S.SettingItems>
-        <S.GPSContainer>
-          <S.GPSTitle>GPS 설정</S.GPSTitle>
-          <S.GPSStateContainer onPress={handleGPSPress}>
-            <S.GPSState
-              style={{
-                color: isGPSEnabled
-                  ? `${SystemColors.success}`
-                  : `${SystemColors.danger}`,
-              }}>
-              {isGPSEnabled ? '되어있음' : '꺼져있음'}
-            </S.GPSState>
-            <S.GPSQuestion source={IC_QUESTION} />
-          </S.GPSStateContainer>
-        </S.GPSContainer>
-        <ThinDivider />
-        <S.PrivacyTermsContainer>
-          <S.PrivacyTermsTitle>개인정보 이용약관</S.PrivacyTermsTitle>
-          <PrivacyPolicyButton />
-        </S.PrivacyTermsContainer>
-        <ThinDivider />
-        <S.AppVersionContainer>
-          <S.AppVersionTitle>앱 버전</S.AppVersionTitle>
-          <S.AppVersionNumber>{appVersion}</S.AppVersionNumber>
-        </S.AppVersionContainer>
-        <ThinDivider />
-        <S.LogoutContainer>
-          <S.LogoutTitle>로그아웃</S.LogoutTitle>
-          <S.LogoutButton source={IC_RIGHT_ARROW} />
-        </S.LogoutContainer>
-        <ThinDivider />
-        <S.DeleteAccountContainer>
-          <S.DeleteAccount onPress={() => navigation.navigate('DeleteAccount')}>
-            회원탈퇴
-          </S.DeleteAccount>
-        </S.DeleteAccountContainer>
+        {withDivider(
+          [
+            <S.GPSContainer>
+              <S.GPSTitle>GPS 설정</S.GPSTitle>
+              <S.GPSStateContainer onPress={handleGPSPress}>
+                <S.GPSState
+                  style={{
+                    color: isGPSEnabled
+                      ? `${SystemColors.success}`
+                      : `${SystemColors.danger}`,
+                  }}>
+                  {isGPSEnabled ? '되어있음' : '꺼져있음'}
+                </S.GPSState>
+                <S.GPSQuestion source={IC_QUESTION} />
+              </S.GPSStateContainer>
+            </S.GPSContainer>,
+
+            <S.PrivacyTermsContainer onPress={handlePrivatePolicyPress}>
+              <S.PrivacyTermsTitle>개인정보 이용약관</S.PrivacyTermsTitle>
+              <S.RightArrowButton source={IC_RIGHT_ARROW} />
+            </S.PrivacyTermsContainer>,
+
+            <S.AppVersionContainer>
+              <S.AppVersionTitle>앱 버전</S.AppVersionTitle>
+              <S.AppVersionNumber>{appVersion}</S.AppVersionNumber>
+            </S.AppVersionContainer>,
+
+            <S.LogoutContainer onPress={handleLogoutPress}>
+              <S.LogoutTitle>로그아웃</S.LogoutTitle>
+              <S.RightArrowButton source={IC_RIGHT_ARROW} />
+            </S.LogoutContainer>,
+
+            <S.DeleteAccountContainer>
+              <S.DeleteAccount
+                onPress={() => navigation.navigate('DeleteAccount')}>
+                회원탈퇴
+              </S.DeleteAccount>
+            </S.DeleteAccountContainer>,
+          ],
+
+          <ThinDivider />,
+        )}
       </S.SettingItems>
     </S.SettingContainer>
   );
