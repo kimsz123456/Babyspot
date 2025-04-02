da2ff66ef30e0233f52ca67a75b40f90e7b29a3d
한병현 <gksqudgus777@naver.com>
2025-04-02 11:05:21 +0900

    feat: Review Response Type 구현 및 getStoreReviews 구현

---
+++ frontend/src/services/reviewService.ts
@@ -0,0 +1,51 @@
+import {api} from './api';
+
+export interface ReviewResponseType {
+  totalElements: number;
+  totalPages: number;
+  pageable: {
+    unpaged: boolean;
+    paged: boolean;
+    pageNumber: number;
+    pageSize: number;
+    sort: SortInfo;
+  };
+  numberOfElements: number;
+  size: number;
+  content: ReviewType[];
+  number: number;
+  sort: SortInfo;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
+
+interface SortInfo {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
+
+export interface ReviewType {
+  reviewId: number;
+  memberId: number;
+  memberNickname: string;
+  babyAge: number[];
+  rating: number;
+  content: string;
+  createdAt: string;
+  imgUrls: string[];
+  likeCount: number;
+  storeId: number;
+}
+
+export const getStoreReviews = async (storeId: number) => {
+  try {
+    const reponse = await api.get(`/reviews/${storeId}/list`);
+    console.log('리뷰 목록 응답:', reponse.data);
+    return reponse.data as ReviewResponseType;
+  } catch (error) {
+    console.error('리뷰 목록 조회 실패:', error);
+    throw error;
+  }
+};
