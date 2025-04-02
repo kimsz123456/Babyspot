4532b4628f4d131d395595de3da02e14c137c07a
한병현 <gksqudgus777@naver.com>
2025-04-02 14:10:44 +0900

    feat: 리뷰리스트 api 연동

--- frontend/src/screens/Map/ReviewListScreen/styles.ts
+++ frontend/src/screens/Map/ReviewListScreen/styles.ts
@@ -64,3 +64,9 @@ export const FilterIconImage = styled(Image)`
 export const ReviewCardListContainer = styled(View)`
   gap: ${scale(8)}px;
 `;
+
+export const NoReviewText = styled(Text)`
+  text-align: center;
+  padding: ${scale(20)}px;
+  color: ${GrayColors[500]};
+`;
