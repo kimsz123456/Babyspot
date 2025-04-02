4358aa56ed209dcbc16d157042b7cc730c710efc
소남주 <skawn1228@gmail.com>
2025-04-02 15:36:04 +0900

    refactor: 추천 페이지 나이 버튼 클릭 시 로직 분리

--- frontend/src/screens/Map/SelectRecommendationAgeScreen/index.tsx
+++ frontend/src/screens/Map/SelectRecommendationAgeScreen/index.tsx
@@ -1,11 +1,15 @@
-import React, {useState} from 'react';
-import * as S from './styles';
+import React, {useEffect, useState} from 'react';
 import {ToastAndroid, View} from 'react-native';
-import AgeButton from '../../../components/atoms/AgeButton';
-import scale from '../../../utils/scale';
-import {AgeProps} from '../ReviewListScreen/ReviewFilterModal';
-import MainButton from '../../../components/atoms/Button/MainButton';
+
 import {useMapNavigation} from '../../../hooks/useNavigationHooks';
+
+import AgeButton from '../../../components/atoms/AgeButton';
+import MainButton from '../../../components/atoms/Button/MainButton';
+import {AgeProps} from '../ReviewListScreen/ReviewFilterModal';
+
+import scale from '../../../utils/scale';
+
+import * as S from './styles';
 
 const MAX_SELECT_COUNT = 3;
 const INITIAL_AGES: AgeProps[] = Array.from({length: 7}, (_, index) => ({
@@ -20,15 +24,37 @@ const SelectRecommendationAgeScreen = () => {
 
   const selectedCount = ages.filter(age => age.isSelected).length;
 
+  const handleAgeButtonPress = (item: AgeProps) => {
+    if (selectedCount >= MAX_SELECT_COUNT && !item.isSelected) {
+      ToastAndroid.show('최대 3개만 선택 가능합니다.', ToastAndroid.SHORT);
+
+      return;
+    }
+
+    setAges(prev => {
+      return prev.map(age => {
+        return age.age === item.age
+          ? {...age, isSelected: !age.isSelected}
+          : age;
+      });
+    });
+  };
+
+  useEffect(() => {
+    console.log(ages);
+  }, [ages]);
+
   return (
     <S.SelectRecommendationAgeScreenContainer>
       <View>
         <S.TextContainer>
-          <S.Title>{`아이들 나이를 바탕으로,\n주변 가게를 추천해드릴게요.`}</S.Title>
+          <S.Title>
+            {'아이들 나이를 바탕으로,\n주변 가게를 추천해드릴게요.'}
+          </S.Title>
           <S.SubTitle $isBold={false}>
-            {`출생 이후 1살인 `}
-            <S.SubTitle $isBold={true}>{`세는 나이`}</S.SubTitle>
-            {`가 기준이에요.`}
+            {'출생 이후 1살인 '}
+            <S.SubTitle $isBold={true}>{'세는 나이'}</S.SubTitle>
+            {'가 기준이에요.'}
           </S.SubTitle>
         </S.TextContainer>
 
@@ -43,31 +69,14 @@ const SelectRecommendationAgeScreen = () => {
               age={item.age}
               isSelected={item.isSelected}
               onPressed={() => {
-                const selectedCount = ages.filter(age => age.isSelected).length;
-
-                if (selectedCount >= MAX_SELECT_COUNT && !item.isSelected) {
-                  ToastAndroid.show(
-                    '최대 3개만 선택 가능합니다.',
-                    ToastAndroid.SHORT,
-                  );
-
-                  return;
-                }
-
-                setAges(prev => {
-                  return prev.map(age => {
-                    return age.age == item.age
-                      ? {...age, isSelected: !age.isSelected}
-                      : age;
-                  });
-                });
+                handleAgeButtonPress(item);
               }}
             />
           )}
         />
       </View>
       <MainButton
-        disabled={selectedCount == 0}
+        disabled={selectedCount === 0}
         text={'추천 받기'}
         onPress={() => {
           navigation.pop();
