4554076de3f03c86d782c0326ac7046247cdca32
소남주 <skawn1228@gmail.com>
2025-04-03 18:03:37 +0900

    feat: chip을 통해 가게 필터링

--- frontend/src/hooks/useChips.ts
+++ frontend/src/hooks/useChips.ts
@@ -1,21 +1,27 @@
-import {useRef, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
-const mockChips: ChipProps[] = [
-  {label: '유아 의자', isSelected: false},
-  {label: '유아 식기', isSelected: false},
-  {label: '기저귀 교환대', isSelected: false},
-  {label: '수유실', isSelected: false},
-  {label: '놀이터', isSelected: false},
-];
+import {useMapStore} from '../stores/mapStore';
 
-interface ChipProps {
+interface ChipType {
   label: string;
+  value: string;
   isSelected: boolean;
 }
 
+const chipList: ChipType[] = [
+  {label: '유아 의자', value: 'babyChair', isSelected: false},
+  {label: '유아 식기', value: 'babyTableware', isSelected: false},
+  {label: '기저귀 교환대', value: 'diaperChangingStation', isSelected: false},
+  {label: '놀이방', value: 'playZone', isSelected: false},
+  {label: '수유실', value: 'nursingRoom', isSelected: false},
+  {label: '6인 이상 테이블', value: 'groupTable', isSelected: false},
+];
+
 const useChips = () => {
-  const initialChips = useRef<ChipProps[]>(mockChips);
-  const [chips, setChips] = useState<ChipProps[]>(mockChips);
+  const initialChips = useRef<ChipType[]>(chipList);
+  const [chips, setChips] = useState<ChipType[]>(chipList);
+
+  const {selectedChips, setSelectedChips} = useMapStore();
 
   const handleChipPressed = (selectedIndex: number) => {
     setChips(prev => {
@@ -31,9 +37,20 @@ const useChips = () => {
           !updated.find(chip => chip.label === initialChip.label)?.isSelected,
       );
 
+      const selectedChipList = selected.map(chip => chip.value);
+      setSelectedChips(selectedChipList);
+
       return [...selected, ...unselected];
     });
   };
+
+  useEffect(() => {
+    if (selectedChips.length > 0) {
+      return;
+    }
+
+    setChips(initialChips.current.map(chip => ({...chip, isSelected: false})));
+  }, [selectedChips]);
 
   return {
     chips,
