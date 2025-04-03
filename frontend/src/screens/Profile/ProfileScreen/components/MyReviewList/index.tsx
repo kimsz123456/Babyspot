17ea3d5a19bebe80f98130e3f53c45bf6aa7319b
한병현 <gksqudgus777@naver.com>
2025-04-03 17:29:37 +0900

    feat: 프로필 화면에서 보여지는 내 리뷰 리스트 api 연동

--- frontend/src/screens/Profile/ProfileScreen/components/MyReviewList/index.tsx
+++ frontend/src/screens/Profile/ProfileScreen/components/MyReviewList/index.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MyReview from '../MyReview';
 import * as S from './styles.ts';
-import mockReviews from '../../mock/mockData.ts';
+import {
+  getMyReviews,
+  ReviewType,
+} from '../../../../../services/reviewService.ts';
 
 export const MyReviewList = () => {
+  const [reviews, setReviews] = useState<ReviewType[]>([]);
+
+  useEffect(() => {
+    const fetchMyReviews = async () => {
+      try {
+        const response = await getMyReviews({
+          page: 0,
+          size: 3,
+        });
+        setReviews(response.content);
+      } catch (error) {
+        throw error;
+      }
+    };
+    fetchMyReviews();
+  }, []);
+
+  if (reviews.length === 0) {
+    return <S.EmptyText>작성한 리뷰가 없습니다.</S.EmptyText>;
+  }
+
   return (
     <S.ReviewListContainer>
-      {mockReviews.map(review => (
-        <MyReview key={review.id} review={review} />
+      {reviews.map(review => (
+        <MyReview key={review.reviewId} review={review} />
       ))}
     </S.ReviewListContainer>
   );
