6b6f210d97b68617361eff0c98b85dcb930b20c8
이상화 <sanghwa1215@naver.com>
2025-04-04 14:26:03 +0900

    refactor : review image 수정

--- backend/src/main/java/com/ssafy/babyspot/api/s3/AwsS3Config.java
+++ backend/src/main/java/com/ssafy/babyspot/api/s3/AwsS3Config.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -13,6 +14,14 @@ public class AwsS3Config {
 	@Bean
 	public S3Presigner s3Presigner() {
 		return S3Presigner.builder()
+			.region(Region.AP_NORTHEAST_2)
+			.credentialsProvider(DefaultCredentialsProvider.create())
+			.build();
+	}
+
+	@Bean
+	public S3Client s3Client() {
+		return S3Client.builder()
 			.region(Region.AP_NORTHEAST_2)
 			.credentialsProvider(DefaultCredentialsProvider.create())
 			.build();
