1fbff22cb58672e4161f3b0f18a80d77bd6f3856
한병현 <gksqudgus777@naver.com>
2025-04-05 17:08:59 +0900

    refactor: 내 리뷰 목록 업데이트 시점 변경

--- frontend/src/navigation/BottomTabNavigator/index.tsx
+++ frontend/src/navigation/BottomTabNavigator/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ProfileScreen from '../../screens/Profile/ProfileScreen';
 import scale from '../../utils/scale';
@@ -7,10 +7,25 @@ import MapStackNavigator from './../MapStackNavigator';
 import ProfileStackNavigator from '../ProfileStackNavigator';
 import BottomTabBarIcon from './BottomTabBarIcon';
 import BottomTabBarLabel from './BottomTabBarLabel';
+import {useGlobalStore} from '../../stores/globalStore';
+import {getMemberProfile} from '../../services/profileService';
+import {Pressable} from 'react-native';
 
 const BottomTabNavigator = () => {
   const Tab = createBottomTabNavigator();
   const insets = useSafeAreaInsets();
+  const {setMemberProfile} = useGlobalStore();
+
+  const handleTabPress = async (route: string) => {
+    if (route === 'Profile') {
+      try {
+        const updatedProfile = await getMemberProfile();
+        setMemberProfile(updatedProfile);
+      } catch (error) {
+        throw error;
+      }
+    }
+  };
 
   return (
     <Tab.Navigator
@@ -24,6 +39,15 @@ const BottomTabNavigator = () => {
         },
         tabBarIcon: ({focused}) => BottomTabBarIcon({focused, route}),
         tabBarLabel: ({focused}) => BottomTabBarLabel({focused, route}),
+        tabBarButton: props => (
+          <Pressable
+            {...props}
+            onPress={e => {
+              handleTabPress(route.name);
+              props.onPress?.(e);
+            }}
+          />
+        ),
       })}>
       <Tab.Screen name="Map" component={MapStackNavigator} />
       <Tab.Screen name="Profile" component={ProfileStackNavigator} />
