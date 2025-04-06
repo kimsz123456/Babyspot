952cd968c03d831f45a78c788acf536cc2dddeec
상한규 <nic6@naver.com>
2025-04-06 21:08:01 +0900

    feature:이미지 뷰어 추가

---
+++ frontend/src/components/atoms/GalleryViewer/styles.ts
@@ -0,0 +1,37 @@
+import {Image, View, TouchableOpacity, Text} from 'react-native';
+import styled from 'styled-components';
+import {GrayColors} from '../../../constants/colors';
+import {FontStyles} from '../../../constants/fonts';
+import scale from '../../../utils/scale';
+
+export const FullscreenImage = styled(Image)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const GalleryHeader = styled(View)`
+  position: absolute;
+  top: ${scale(40)}px;
+  left: 0;
+  right: 0;
+  z-index: 10;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px ${scale(24)}px;
+  background-color: linear-gradient(rgba(0, 0, 0, 0.6), transparent);
+`;
+
+export const CloseButton = styled(TouchableOpacity)`
+  padding: 8px;
+`;
+
+export const CloseIconImage = styled(Image)`
+  width: ${scale(24)}px;
+  height: ${scale(24)}px;
+`;
+
+export const IndexText = styled(Text)`
+  ${FontStyles.captionMedium};
+  color: ${GrayColors[300]};
+`;
