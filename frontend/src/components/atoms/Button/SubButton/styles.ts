5906505bf6dc2815683ab202ee3adda9ed17de77
상한규 <nic6@naver.com>
2025-04-07 16:39:08 +0900

    feature:닉네임 중복 확인

--- frontend/src/components/atoms/Button/SubButton/styles.ts
+++ frontend/src/components/atoms/Button/SubButton/styles.ts
@@ -4,17 +4,23 @@ import {GrayColors} from '../../../../constants/colors';
 import {FontStyles} from '../../../../constants/fonts';
 import scale from '../../../../utils/scale';
 
-const SubButtonContainer = styled(TouchableOpacity)<{$color: string}>`
+interface SubButtonProps {
+  $disabled?: boolean;
+  $color?: string;
+}
+
+const SubButtonContainer = styled(TouchableOpacity)<SubButtonProps>`
   width: 100%;
   border-radius: 10px;
-  border-color: ${({$color}) => $color};
+  border-color: ${({$color, $disabled}) =>
+    $disabled ? GrayColors[500] : $color};
   border-width: ${scale(1)}px;
   background-color: ${GrayColors[0]};
   align-items: center;
 `;
-const SubButtonText = styled(Text)<{$color: string}>`
+const SubButtonText = styled(Text)<SubButtonProps>`
   ${FontStyles.bodyMedium};
-  color: ${({$color}) => $color};
+  color: ${({$color, $disabled}) => ($disabled ? GrayColors[500] : $color)};
   padding: ${scale(16)}px 0;
 `;
 
