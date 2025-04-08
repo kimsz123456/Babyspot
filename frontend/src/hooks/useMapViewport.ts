70a3eecc9ef936b2d86b4cb7c72db70e602b04bb
소남주 <skawn1228@gmail.com>
2025-04-08 19:53:49 +0900

    refactor: 음식점 기본 정보 관련 로직 전역으로 분리

--- frontend/src/hooks/useMapViewport.ts
+++ frontend/src/hooks/useMapViewport.ts
@@ -1,10 +1,7 @@
 import {useState} from 'react';
 
 import {Camera, Region} from '@mj-studio/react-native-naver-map';
-import {
-  INITIAL_MAP_CENTER_COORDINATE,
-  MAP_ZOOM_SCALE,
-} from '../constants/constants';
+import {MAP_ZOOM_SCALE} from '../constants/constants';
 import {useMapStore} from '../stores/mapStore';
 
 const useMapViewport = () => {
@@ -14,7 +11,7 @@ const useMapViewport = () => {
   });
   const [zoom, setZoom] = useState<number | undefined>(MAP_ZOOM_SCALE.basic);
 
-  const {centerCoordinate, setCenterCoordinate} = useMapStore();
+  const {setCenterCoordinate} = useMapStore();
 
   const onCameraIdle = (e: Camera & {region: Region}) => {
     setCenterCoordinate({
@@ -31,7 +28,6 @@ const useMapViewport = () => {
   };
 
   return {
-    centerCoordinate,
     mapRegion,
     zoom,
     onCameraIdle,
--- frontend/src/stores/mapStore.ts
+++ frontend/src/stores/mapStore.ts
@@ -1,10 +1,25 @@
 import {create} from 'zustand';
-import {GetGeocodingByKeywordResponse} from '../services/mapService';
+
+import {
+  ConvenienceType,
+  StoreBasicInformationType,
+} from '../screens/Map/NearStoreListScreen/components/StoreBasicInformation/types';
+import {
+  GetGeocodingByKeywordResponse,
+  getRangeInfo,
+} from '../services/mapService';
+
+import calculateMapRegion from '../utils/calculateMapRegion';
 import {INITIAL_MAP_CENTER_COORDINATE} from '../constants/constants';
 
 export interface Coordinate {
   latitude: number;
   longitude: number;
+}
+
+interface MapRegionType {
+  latitudeDelta: number;
+  longitudeDelta: number;
 }
 
 interface MapState {
@@ -20,6 +35,14 @@ interface MapState {
   centerCoordinate: Coordinate;
   setCenterCoordinate: (centerCoordinate: Coordinate) => void;
 
+  storeBasicInformation: StoreBasicInformationType[];
+  fetchStoreBasicInformation: (mapRegion: MapRegionType) => Promise<void>;
+
+  filteredStoreBasicInformation: StoreBasicInformationType[];
+  setFilteredStoreBasicInformation: () => void;
+
+  isLoading: boolean;
+
   clearSelectedPlace: () => void;
   resetMapState: () => void;
 }
@@ -29,9 +52,12 @@ const initialState = {
   selectedAges: [],
   selectedChips: [],
   centerCoordinate: INITIAL_MAP_CENTER_COORDINATE,
+  storeBasicInformation: [],
+  filteredStoreBasicInformation: [],
+  isLoading: false,
 };
 
-export const useMapStore = create<MapState>(set => ({
+export const useMapStore = create<MapState>((set, get) => ({
   ...initialState,
   setSelectedPlace: selectedPlace => set({selectedPlace: selectedPlace}),
 
@@ -41,6 +67,79 @@ export const useMapStore = create<MapState>(set => ({
 
   setCenterCoordinate: centerCoordinate =>
     set({centerCoordinate: centerCoordinate}),
+
+  fetchStoreBasicInformation: async mapRegion => {
+    try {
+      const {selectedAges, centerCoordinate} = get();
+
+      const {topLeft, bottomRight} = calculateMapRegion(
+        centerCoordinate,
+        mapRegion,
+      );
+
+      set({isLoading: true});
+
+      const stores = await getRangeInfo({
+        topLeftLat: topLeft.latitude,
+        topLeftLong: topLeft.longitude,
+        bottomRightLat: bottomRight.latitude,
+        bottomRightLong: bottomRight.longitude,
+      });
+
+      if (selectedAges?.length === 0) {
+        set({storeBasicInformation: stores});
+
+        return;
+      }
+
+      const recommendStores = stores
+        .filter(store =>
+          store.babyAges?.some((age: number) => selectedAges.includes(age)),
+        )
+        .map(store => {
+          const matchCount =
+            store.babyAges?.filter((age: number) => selectedAges.includes(age))
+              .length ?? 0;
+
+          return {
+            ...store,
+            matchCount,
+          };
+        })
+        .sort((a, b) => b.matchCount - a.matchCount);
+
+      set({storeBasicInformation: recommendStores});
+    } catch (error) {
+      throw new Error('주변 가게 검색 중 문제가 발생하였습니다.');
+    } finally {
+      set({isLoading: false});
+    }
+  },
+
+  setFilteredStoreBasicInformation: () => {
+    const {storeBasicInformation, selectedChips} = get();
+
+    let filteredStores: StoreBasicInformationType[];
+
+    if (selectedChips.length === 0) {
+      filteredStores = storeBasicInformation;
+    }
+
+    filteredStores = storeBasicInformation.filter(store => {
+      const conveniences = store.convenience[0].convenienceDetails;
+
+      if (!conveniences) {
+        return false;
+      }
+
+      return selectedChips.every(
+        chip =>
+          conveniences[chip as keyof ConvenienceType['convenienceDetails']],
+      );
+    });
+
+    set({filteredStoreBasicInformation: filteredStores});
+  },
 
   clearSelectedPlace: () => set({selectedPlace: null}),
   resetMapState: () => set({...initialState}),
