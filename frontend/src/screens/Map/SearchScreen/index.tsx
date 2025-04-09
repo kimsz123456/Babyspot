b58b76f104a5b857ed6508450b4ea514661653d9
상한규 <nic6@naver.com>
2025-04-09 14:57:08 +0900

    fix: 검색창 밖 눌렀을 때 포커스 해제

--- frontend/src/screens/Map/SearchScreen/index.tsx
+++ frontend/src/screens/Map/SearchScreen/index.tsx
@@ -3,7 +3,12 @@ import SearchHistory from './components/SearchHistory';
 import * as S from './styles';
 import NoDataContainer from '../../../components/atoms/NoDataContainer';
 import {withDivider} from '../../../utils/withDivider';
-import {FlatList, TextInput} from 'react-native';
+import {
+  FlatList,
+  Keyboard,
+  TextInput,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import {PrimaryColors, GrayColors} from '../../../constants/colors';
 import {IC_LEFT_ARROW} from '../../../constants/icons';
 import {useMapNavigation} from '../../../hooks/useNavigationHooks';
@@ -68,66 +73,68 @@ const SearchScreen = () => {
   };
 
   return (
-    <S.SearchScreenContainer>
-      <S.PlaceSearchButton>
-        <S.LeftArrowIconContainer
-          onPress={() => {
-            navigation.goBack();
-          }}>
-          <S.LeftArrowIcon source={IC_LEFT_ARROW} />
-        </S.LeftArrowIconContainer>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <S.SearchScreenContainer>
+        <S.PlaceSearchButton>
+          <S.LeftArrowIconContainer
+            onPress={() => {
+              navigation.goBack();
+            }}>
+            <S.LeftArrowIcon source={IC_LEFT_ARROW} />
+          </S.LeftArrowIconContainer>
 
-        <TextInput
-          value={searchText}
-          placeholder={'검색할 장소를 입력해주세요'}
-          selectionColor={PrimaryColors[500]}
-          placeholderTextColor={GrayColors[300]}
-          style={{
-            flex: 1,
-            padding: scale(8),
-            margin: 0,
-            fontSize: scale(16),
-            fontFamily: 'Pretendard-Regular',
-            lineHeight: scale(24),
-            fontWeight: 400,
-            letterSpacing: scaleLetterSpacing(16, -2),
-            includeFontPadding: false,
-            textDecorationColor: GrayColors[800],
-          }}
-          onChangeText={text => {
-            searchPlaces(text);
-          }}
-        />
-      </S.PlaceSearchButton>
+          <TextInput
+            value={searchText}
+            placeholder={'검색할 장소를 입력해주세요'}
+            selectionColor={PrimaryColors[500]}
+            placeholderTextColor={GrayColors[300]}
+            style={{
+              flex: 1,
+              padding: scale(8),
+              margin: 0,
+              fontSize: scale(16),
+              fontFamily: 'Pretendard-Regular',
+              lineHeight: scale(24),
+              fontWeight: 400,
+              letterSpacing: scaleLetterSpacing(16, -2),
+              includeFontPadding: false,
+              textDecorationColor: GrayColors[800],
+            }}
+            autoFocus={true}
+            onChangeText={text => {
+              searchPlaces(text);
+            }}
+          />
+        </S.PlaceSearchButton>
 
-      {searchText.trim().length != 0 ? (
-        <FlatList
-          data={results}
-          keyExtractor={item => item.id.toString()}
-          showsVerticalScrollIndicator={false}
-          ListEmptyComponent={
-            <NoDataContainer text={'검색 결과가 없습니다.'} />
-          }
-          renderItem={({item, index}) => (
-            <S.PlaceButtonTile
-              $showLine={index != results.length - 1}
-              onPress={() => handleSelect(item)}>
-              <S.PlaceNameText>
-                {renderHighlightedName(item.place_name, searchText, index)}
-              </S.PlaceNameText>
-              <S.PlaceAddressText>
-                {item.road_address_name || item.address_name}
-              </S.PlaceAddressText>
-            </S.PlaceButtonTile>
-          )}
-          keyboardShouldPersistTaps="handled"
-        />
-      ) : (
-        <S.SearchHistoryListContainer>
-          <S.SearchHistoryListTitle>최근 검색</S.SearchHistoryListTitle>
-          <S.SearchHistoryList>
-            <NoDataContainer text="서비스 준비 중입니다." />
-            {/* {withDivider(
+        {searchText.trim().length != 0 ? (
+          <FlatList
+            data={results}
+            keyExtractor={item => item.id.toString()}
+            showsVerticalScrollIndicator={false}
+            ListEmptyComponent={
+              <NoDataContainer text={'검색 결과가 없습니다.'} />
+            }
+            renderItem={({item, index}) => (
+              <S.PlaceButtonTile
+                $showLine={index != results.length - 1}
+                onPress={() => handleSelect(item)}>
+                <S.PlaceNameText>
+                  {renderHighlightedName(item.place_name, searchText, index)}
+                </S.PlaceNameText>
+                <S.PlaceAddressText>
+                  {item.road_address_name || item.address_name}
+                </S.PlaceAddressText>
+              </S.PlaceButtonTile>
+            )}
+            keyboardShouldPersistTaps="handled"
+          />
+        ) : (
+          <S.SearchHistoryListContainer>
+            <S.SearchHistoryListTitle>최근 검색</S.SearchHistoryListTitle>
+            <S.SearchHistoryList>
+              <NoDataContainer text="서비스 준비 중입니다." />
+              {/* {withDivider(
             [
               ...[].map((history, index) => (
                 <SearchHistory
@@ -139,10 +146,11 @@ const SearchScreen = () => {
             ],
             <S.Divider />,
           )} */}
-          </S.SearchHistoryList>
-        </S.SearchHistoryListContainer>
-      )}
-    </S.SearchScreenContainer>
+            </S.SearchHistoryList>
+          </S.SearchHistoryListContainer>
+        )}
+      </S.SearchScreenContainer>
+    </TouchableWithoutFeedback>
   );
 };
 
