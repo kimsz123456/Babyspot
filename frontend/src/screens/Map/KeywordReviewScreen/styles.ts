1aa1fcfb027adf3fe2ac3f7d96653183326a2434
한병현 <gksqudgus777@naver.com>
2025-04-09 16:51:49 +0900

    refactor: border radius 통일

--- frontend/src/screens/Map/KeywordReviewScreen/styles.ts
+++ frontend/src/screens/Map/KeywordReviewScreen/styles.ts
@@ -46,6 +46,7 @@ export const ReviewContainer = styled(TouchableOpacity)`
 export const IconImage = styled(Image)`
   width: ${scale(26)}px;
   height: ${scale(26)}px;
+  border-radius: ${scale(5)}px;
 `;
 export const ContentText = styled(Text)`
   flex: 1;
