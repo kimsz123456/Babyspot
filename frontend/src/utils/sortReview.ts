0a11d3dca2dc08815e285f7c4c6c78cbfab44958
한병현 <gksqudgus777@naver.com>
2025-04-09 10:54:26 +0900

    refactor: 리뷰에서 사진 확대가 가능하게 설정

--- frontend/src/utils/sortReview.ts
+++ frontend/src/utils/sortReview.ts
@@ -1,7 +1,10 @@
 import {ReviewType} from '../services/reviewService';
 
-export const sortReview = (reviews: ReviewType[]) => {
-  return reviews.sort((a, b) => {
+export const sortReview = (reviews: ReviewType[] | undefined) => {
+  if (!reviews) {
+    return [];
+  }
+  return [...reviews].sort((a, b) => {
     const dateA = new Date(a.createdAt).getTime();
     const dateB = new Date(b.createdAt).getTime();
     return dateB - dateA;
